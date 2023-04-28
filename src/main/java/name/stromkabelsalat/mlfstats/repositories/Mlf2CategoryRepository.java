466a1413685d5423aa83dc92bc1687a1b4073836
stromkabelsalat <stromkabelsalat@ident.email>
2023-04-28 14:50:07 +0200

    Implement original application

---
+++ src/main/java/name/stromkabelsalat/mlfstats/repositories/Mlf2CategoryRepository.java
@@ -0,0 +1,8 @@
+package name.stromkabelsalat.mlfstats.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import name.stromkabelsalat.mlfstats.entities.Mlf2Category;
+
+public interface Mlf2CategoryRepository extends JpaRepository<Mlf2Category, Integer> {
+}
